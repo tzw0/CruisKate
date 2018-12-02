5848a5dc0041233aa334db8ddffde3047f6386ac
tzw0 <42692203+tzw0@users.noreply.github.com>
2018-12-02 23:39:07 +0800

    Create basic_bt_led.ino

---
+++ basic_bt_led.ino
@@ -0,0 +1,25 @@
+/* use arduino bluetooth controller app/ bt remote, command <1> on led, <0> off led
+int state = 0;
+#define ledPin 7
+void setup(){
+pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(9600); // Default communication rate of the Bluetooth module
+}
+
+void loop() {
+   if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+    state = Serial.read(); // Reads the data from the serial port
+    Serial.println(state);
+ }
+ if (state == '0') {
+  digitalWrite(ledPin, LOW); // Turn LED OFF
+  //Serial.println("LED: OFF"); // Send back, to the phone, the String "LED: ON"
+  state = 0;
+ }
+ else if (state == '1') {
+  digitalWrite(ledPin, HIGH);
+  //Serial.println("LED: ON");;
+  state = 0;
+ }  
+}
